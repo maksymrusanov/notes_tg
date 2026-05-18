56ddd8cb7349f58e6d67e9f190e919e25d6fac5b
Maksym <1maksymrusanov1@gmail.com>
2026-05-18 21:14:29 +0100

    finished bot logic

--- bot/handlers.py
+++ bot/handlers.py
@@ -18,13 +18,13 @@ async def start(message: Message):
 
 
 @router.callback_query(F.data == "New")
-async def add_note(message: Message, state: FSMContext):
+async def add_note(callback_query: CallbackQuery, state: FSMContext):
     await state.set_state(Notes.name)
-    await message.answer(text="Write your note name")
+    await callback_query.message.answer(text="Write your note name")
 
 
 @router.message(Notes.name)
-async def save_name(message: Message, state: FSMContext):
+async def save_name(message, state: FSMContext):
     await state.update_data(name=message.text)
     await state.set_state(Notes.description)
     await message.answer("write your note description")
@@ -38,16 +38,56 @@ async def save_note(message: Message, state: FSMContext):
     await db.add_note(
         user_id=data["user_id"], name=data["name"], description=data["description"]
     )
-    await message.answer("Your note was added")
+    await message.answer(
+        "Your note was added\nWhat else you want to do? ", reply_markup=ga
+    )
+
+
+@router.callback_query(F.data == "All")
+async def get_all(callback: CallbackQuery):
+    all_notes = await db.get_all_notes()
+    if not all_notes:
+        await callback.message.answer(
+            text="No notes found.\nWhat else you want to do?", reply_markup=ga
+        )
+    else:
+        for i in all_notes:
+            await callback.message.answer(
+                text=f"ID:{i[0]} \nName:{i[1]}\nDescription: {i[2]}\nStatus: {i[3]}",
+                reply_markup=na,
+            )
 
 
 @router.callback_query(F.data == "In progress")
 async def get_in_progress(callback: CallbackQuery):
     notes = await db.get_notes(status="In progress")
     if not notes:
-        await callback.message.answer("No 'in progress' notes found.")
+        await callback.message.answer(
+            text="No 'in progress' notes found.\nwhat you want to do?", reply_markup=ga
+        )
     else:
         for note in notes:
             await callback.message.answer(
-                text=f"Name: {note[0]}, Description: {note[1]}", reply_markup=na
+                text=f"ID:{note[0]}\nName:{note[1]}\nDescription: {note[2]}\nStatus: {note[3]}",
+                reply_markup=na,
             )
+    await callback.answer(text="what you want to do?", reply_markup=ga)
+
+
+@router.callback_query(F.data == "mark_done")
+async def mark_as_done(callback: CallbackQuery):
+    note_id = int(callback.message.text.split()[0][3:])
+    await db.change_note_status(note_id=note_id, new_status="Done")
+    await callback.message.answer("Note marked as done", reply_markup=ga)
+
+
+@router.callback_query(F.data == "Back")
+async def back(callback: CallbackQuery):
+    await callback.message.answer(text="what you want to do?", reply_markup=ga)
+
+
+@router.callback_query(F.data == "Delete")
+async def delete_note(callback: CallbackQuery):
+    note_id = int(callback.message.text.split()[0][3:])
+    await db.delete_note(note_id=note_id)
+    await callback.message.answer("Note deleted", reply_markup=ga)
--- bot/keyboards.py
+++ bot/keyboards.py
@@ -3,18 +3,16 @@ from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 general_actions = InlineKeyboardMarkup(
     inline_keyboard=[
         [InlineKeyboardButton(text="Create new note", callback_data="New")],
-        [
-            InlineKeyboardButton(
-                text="Check 'in progress'  ", callback_data="In progress"
-            )
-        ],
-        [InlineKeyboardButton(text="Mark as done", callback_data="Done")],
-        [InlineKeyboardButton(text="Set time", callback_data="time")],
+        [InlineKeyboardButton(text="Check 'in progress'",
+                              callback_data="In progress")],
+        [InlineKeyboardButton(text="All notes", callback_data="All")],
     ]
 )
 note_actions = InlineKeyboardMarkup(
     inline_keyboard=[
         [InlineKeyboardButton(text="Mark as done", callback_data="mark_done")],
         [InlineKeyboardButton(text="Set time", callback_data="set_time")],
+        [InlineKeyboardButton(text="Delete", callback_data="Delete")],
+        [InlineKeyboardButton(text="Back", callback_data="Back")],
     ]
 )
